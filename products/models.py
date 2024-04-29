a899057749f7bdb72149cd509fbf0dc8030a67b1
gozzun <gozzun_1256@naver.com>
2024-04-29 19:52:34 +0900

    상품 등록, 목록 조회 성공

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image_url = models.CharField(max_length=200, validators=[URLValidator()])
+
+    def clean(self):
+        super().clean()
+        if not self.image_url.startswith("http://") and not self.image_url.startswith("https://"):
+            raise ValidationError("Invalid image URL.")
--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status,generics
+from rest_framework.pagination import PageNumberPagination
 
 # Create your views here.
+class ProductListAPIView(generics.ListCreateAPIView):
+# class ProductListAPIView(APIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+
+    @permission_classes([IsAuthenticated])
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    # def get(self,request):
+    #     products = Product.objects.all()
+    #     serializer = ProductSerializer(products, many=True)
+    #     return Response(serializer.data)
+
+class ProductDetailAPIView(APIView):
+    pass
